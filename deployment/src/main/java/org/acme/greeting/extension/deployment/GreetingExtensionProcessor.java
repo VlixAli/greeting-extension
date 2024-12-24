a28f0395dd265c290beb56cfba6dc0fc2c9bedd8
Ali Eldiasty <qali372@gmail.com>
2024-12-24 12:57:10 +0200

    building my first quarkus extension

---
+++ deployment/src/main/java/org/acme/greeting/extension/deployment/GreetingExtensionProcessor.java
@@ -0,0 +1,23 @@
+package org.acme.greeting.extension.deployment;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.undertow.deployment.ServletBuildItem;
+import org.acme.greeting.extension.GreetingExtensionServlet;
+
+class GreetingExtensionProcessor {
+
+    private static final String FEATURE = "greeting-extension";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ServletBuildItem createServlet() {
+        return ServletBuildItem.builder(FEATURE, GreetingExtensionServlet.class.getName())
+                .addMapping("/greeting")
+                .build();
+    }
+}
